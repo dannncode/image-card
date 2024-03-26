b04520e0d9d9d2d9074156e0b7cf9a6798c1d21d
Danilo Lucban <danilolucban09@gmail.com>
2024-03-26 18:49:28 +0800

    first commit

---
+++ Hello World/main.js
@@ -0,0 +1,113 @@
+//console.log("hello world!");
+
+// alert("hello");
+
+// document.getElementById("p1").innerHTML = "hello";
+// document.getElementById("p1").style.color = "red";
+// document.getElementById("p1").style.textAlign = "center";
+
+
+// function myFunction() {
+//     document.getElementById("p1").style.color = "blue";
+//     document.getElementById("p1").innerText = "hi";
+//     document.getElementById("p1").style.background = "lightpink";
+// }
+
+// let a, b, c;
+// a = 100;
+// b = 200;
+// c = a + b;
+
+// console.log(c);
+
+// var name = "danilo";
+
+// console.log(name)
+
+// var name = "lyn"
+
+// console.log(name)
+
+// let age = 10;
+// console.log(age);
+
+// let age = 20;
+// console.log(age);
+
+// let = a, b, c;
+// a = "banana";
+// b = 100;
+// c = a + b;
+// console.log(c)
+
+// var fruits;
+// fruits = "banana";
+// console.log(fruits);
+
+// const carName = "honda";
+// console.log(carName)
+
+//Automaticlly
+x = 100;
+y = 200;
+z = x + y;
+
+console.log(z);
+
+//var
+var x = 1000;
+var y = 2000;
+var z = x + y;
+
+console.log(z);
+
+//let
+let firstName = "dannn";
+let lastName = "code";
+let fullName = firstName + lastName;
+
+console.log(fullName);
+
+const age = 20;
+const age1 = 30;
+const age2 = age - age1;
+
+console.log(age2);
+
+const payment1 = 100;
+const payment2 = 200;
+
+let change = payment1 * payment2;
+
+console.log(change);
+
+let a = 200;
+let b = 200;
+let c = a + b;
+
+console.log(c);
+
+let placeName = "tagaytay";
+console.log(typeof(placeName));
+
+let placeHome = "world!";
+document.getElementById("p2").innerHTML = placeHome;
+
+
+let manName = "carlo", manAge = 20, manCar = "toyota";
+console.log(manName, manAge, manCar)
+
+namePlant = "sampaguita";
+var namePlant;
+
+console.log(namePlant);
+
+
+let nameRiver = 'wawa';
+
+console.log(nameRiver);
+
+let nameSea;
+nameSea = "pacific ocean";
+
+console.log(nameSea);
